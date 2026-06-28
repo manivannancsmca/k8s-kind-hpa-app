4201aebe4855a8c5d7d0a02eb2467dc6a946df26
manivannancsmca <manivannancsmca@gmail.com>
2026-06-28 10:22:23 +0530

    swagger dependency added and employee model class added

---
+++ src/main/java/com/k8s_kind_hpa_app/model/Employee.java
@@ -0,0 +1,30 @@
+package com.k8s_kind_hpa_app.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String department;
+}
