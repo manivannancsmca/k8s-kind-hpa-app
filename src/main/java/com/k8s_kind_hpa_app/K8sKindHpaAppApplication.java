cf2a7edf2ab7695c5586f048f33035397d90a636
manivannancsmca <manivannancsmca@gmail.com>
2026-06-28 10:00:19 +0530

    first commit

---
+++ src/main/java/com/k8s_kind_hpa_app/K8sKindHpaAppApplication.java
@@ -0,0 +1,13 @@
+package com.k8s_kind_hpa_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sKindHpaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sKindHpaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/k8s_kind_hpa_app/K8sKindHpaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k8s_kind_hpa_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sKindHpaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
